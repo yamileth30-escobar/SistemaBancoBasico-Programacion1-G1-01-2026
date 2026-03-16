1420d1ca0a7daf87330d40968909ba4d3ceb0e1b
yamileth30-escobar <ve1105032024@unab.edu.sv>
2026-03-16 11:18:33 -0600

    Add files via upload

---
+++ EL/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+    public class Cliente
+    {
+        // propiedades -> prop
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Documento { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
---
+++ EL/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+    public class Enums
+    {
+        // Estructura de datos llamada Enum
+
+        public enum EstadoCuenta : byte
+        {
+            Activa=1,
+            Bloqueada=2,
+            Cerrada=3
+        }
+    }
+}
